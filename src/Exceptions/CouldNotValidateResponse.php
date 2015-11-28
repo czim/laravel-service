92f3ca7268f08de35a962c3f32a66211a90e41a7
Coen Zimmerman <coen@pixelindustries.com>
2015-11-28 12:32:54 +0100

    added specific exception for validation errors

---
+++ src/Exceptions/CouldNotValidateResponse.php
@@ -0,0 +1,10 @@
+<?php
+namespace Czim\Service\Exceptions;
+
+/**
+ * Validation exception thrown during interpretation of response.
+ * See Interpreters\Decorators.
+ */
+class CouldNotValidateResponse extends CouldNotInterpretResponse
+{
+}
